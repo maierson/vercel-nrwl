5be207f8e8e547103ca370b023183165f733046f
maierson <dahn@maierinc.com>
2021-06-15 22:26:53 -0400

    Initial commit

--- apps/vercel-deploy-e2e/src/integration/app.spec.ts
+++ apps/vercel-deploy-e2e/src/integration/app.spec.ts
@@ -1,6 +1,6 @@
 import { getGreeting } from '../support/app.po';
 
-describe('vercel', () => {
+describe('vercel-deploy', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
@@ -8,6 +8,6 @@ describe('vercel', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to vercel!');
+    getGreeting().contains('Welcome to vercel-deploy!');
   });
 });
