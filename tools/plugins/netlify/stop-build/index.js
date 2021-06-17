fae68383162ec9dab3f98e1c6ed4d659cac6a39d
maierson <dahn@maierinc.com>
2021-06-17 15:16:19 -0400

    update plugin

--- tools/plugins/netlify/stop-build/index.js
+++ tools/plugins/netlify/stop-build/index.js
@@ -4,11 +4,12 @@ module.exports = {
   onPreBuild: () => {
     console.log('Hello world from the plugin!');
     console.log('Dir', process.cwd());
+    console.log('Dirs:', fs.readdirSync(process.cwd()));
   },
   onEnd: () => {
     const currentProject = process.env.PROJECT_NAME;
     console.log('--- Build End ---\n');
     console.log('Project Name:', currentProject);
-    console.log('Dirs:', fs.readdirSync(`${process.cwd()}/dist"));
+    console.log('Dirs:', fs.readdirSync(`${process.cwd()}/dist`));
   },
 };
